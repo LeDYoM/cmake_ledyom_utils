cab69be6f1537156d3b7bd29c453f7118c57ca06
Ismael Gonzalez <ledyom@hotmail.com>
2026-09-09 18:06:18 +0200

    Start documentation script

---
+++ documenting.cmake
@@ -0,0 +1,22 @@
+
+function(build_docs target_list)
+  set(DOXYGEN_GENERATE_HTML YES)
+  set(DOXYGEN_EXCLUDE build;tests)
+  set(DOXYGEN_EXCLUDE_PATTERNS
+    */.git/*
+    */build/*
+  */tests/*)
+  set(DOXYGEN_USE_MDFILE_AS_MAINPAGE README.md)
+
+  find_package(Doxygen)
+
+  if (Doxygen_FOUND)
+    doxygen_add_docs(
+        docs
+        ${target_list} README.md
+        COMMENT "Generate html pages for the framework"
+    )
+  else()
+    message("Doxygen not found!")
+  endif()
+endfunction()
